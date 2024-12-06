46cfea6ac24976e0daed12ba267c1d6b49f962d2
ziyang22 <zhangweihao22@outlook.com>
2024-12-06 13:11:43 +0800

    Update run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/powershell
+# 转到build目录下
+cd build
+# 指定生成器为 MinGW Makefiles
+# MinGW是windos 下的一个开发环境
+# 所以你使用时需要确保你已经安装了MINBGW
+cmake -G "MinGW Makefiles"..
+# 使用 mingw32-make 构建项目
+mingw32-make
+# 运行生成的exe文件
+./app
